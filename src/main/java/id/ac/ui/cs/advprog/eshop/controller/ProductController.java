aec0470313d860b0a245e2d75d951d50751ff6cf
qqeel <ahmadaqeelknx@gmail.com>
2026-02-10 19:31:26 +0700

    Completed product list function

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/products")
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
