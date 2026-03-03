87144d55e1d47feeb23fcc1fa2e668f4c872ea79
qqeel <ahmadaqeelknx@gmail.com>
2026-03-03 20:25:59 +0700

    Implemented SOLID principles to the code

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -37,7 +37,7 @@ public class CarServiceImpl implements CarService{
     }
 
     @Override
-    public void deleteCarById(String carId) {
+    public void deleteById(String carId) {
         carRepository.deleteById(carId);
     }
 }
